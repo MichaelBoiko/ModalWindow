db703c5d6ec571a89f562c8fd9f846ef505aaf09
Michael Boiko <mchlboiko@gmail.com>
2024-05-12 16:34:59 +0300

    some changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-$.modal();
+const modal = $.modal({
+    title: 'Modal Window',
+    closable: true,
+    content: `
+        <p>Modal is working</p>
+    `,
+    width: '400px'
+});
+
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,31 +1,50 @@
 function _createModal(options) {
+    const DEFAULT_WIDTH = '600px';
     const modal = document.createElement('div');
+
     modal.classList.add('emodal');
     modal.insertAdjacentHTML('afterbegin', `
-        <div class="emodal-overlay">
-            <div class="modal-window">
+        <div class="modal-overlay" data-close="true">
+            <div class="modal-window" style="${options.with || DEFAULT_WIDTH}">
                 <div class="modal-header">
-                    <span class="modal-title">modal-title</span>
-                    <span class="modal-close">&times;</span>
+                    <span class="modal-title">${options.title || 'Окно'}</span>
+                    ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
                 </div>
                 <div class="modal-body">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet.</p>
+                    ${options.content || ''}
                 </div>
                 <div class="modal-footer">
-                    <button>Ok</button>
-                    <button>Cancel</button>
+                    <button type="button" class="btn btn-success" data-close="true">Ok</button>
+                    <button type="button" class="btn btn-danger" data-close="true">Cancel</button>
                 </div>
             </div>
         </div>
     `);
+    document.body.append(modal);
     return modal;
 }
 
 $.modal = function(options) {
-    return {
-        open() {},
-        close() {},
-        destroy() {},
+    const $modal = _createModal(options);
+    
+    const modalObj = {
+        open() {
+            $modal.classList.add('open');
+        },
+        close() {
+            $modal.classList.remove('open');
+            $modal.classList.add('hide');
+            setTimeout(() => {
+                $modal.classList.remove('hide');
+            }, 200);
+        },
     }
+
+    $modal.addEventListener('click', event => {
+        if(event.target.dataset.close){
+            modalObj.close();
+        }
+    })
+
+    return modalObj;
 }
